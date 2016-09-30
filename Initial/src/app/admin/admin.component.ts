0c8a1419e6a6995315453c5f4c5347b46db09084
Kotaru <110190@CTS.COM>
2016-10-01 01:17:03 +0530

    Firebase ready code.

--- Initial/src/app/admin/admin.component.ts
+++ Initial/src/app/admin/admin.component.ts
@@ -1,13 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import {BusDataAccessService} from '../shared/bus-data-access.service'
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.css']
+  styleUrls: ['./admin.component.css'],
+  providers: [BusDataAccessService]
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  buses: Array<any>;
+  constructor(private busDataAccess: BusDataAccessService) 
+  {
+    this.buses = busDataAccess.getBusData()
+  }
+
+  increment(item){
+       item.delay += 1;
+       item.expectedTimeOfArrival = this.addMinutes(item, 1);
+   }
+
+   decrement(item){
+     if(item.delay > 0){
+     
+      item.delay -= 1;
+      item.expectedTimeOfArrival = this.deductMinutes(item, 1);
+     }
+   }
+
+   addMinutes(item, minutes){
+     let lTime: Date = new Date(item.expectedTimeOfArrival);
+     lTime.setMinutes(lTime.getMinutes() + minutes);
+     return lTime;
+   }
+
+   deductMinutes(item, minutes){
+     let lTime: Date = new Date(item.expectedTimeOfArrival);
+     lTime.setMinutes(lTime.getMinutes() - minutes);
+     return lTime;
+   }
+
+   save(item){
+    console.log(item.delayReason);
+   }
 
   ngOnInit() {
   }
--- Initial/src/app/bus-list/bus-list.component.ts
+++ Initial/src/app/bus-list/bus-list.component.ts
@@ -1,13 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import {BusDataAccessService} from '../shared/bus-data-access.service'
 
 @Component({
   selector: 'app-bus-list',
   templateUrl: './bus-list.component.html',
-  styleUrls: ['./bus-list.component.css']
+  styleUrls: ['./bus-list.component.css'],
+  providers: [BusDataAccessService]
 })
 export class BusListComponent implements OnInit {
 
-  constructor() { }
+  buses: Array<any>;
+  constructor(private busDataAccess: BusDataAccessService) 
+  {
+    this.buses = busDataAccess.getBusData()
+   }
 
   ngOnInit() {
   }
